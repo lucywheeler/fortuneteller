40a5101e90786324167ab6a598ec4e72cdd37112
Lucy Wheeler <willie.wheeler@gmail.com>
2016-04-30 04:13:06 +0000

    initial commit

---
+++ FortuneTeller.py
@@ -0,0 +1,25 @@
+import random
+
+
+
+fortunes = [
+  "i love lucy lucy is very very Nice!",
+  "you are amazing just be you and have fun!",
+  "I love Ben. Ben is the nicest boy on the planet!",
+  "max & Ruby is my favorite show!",
+  "I love ark survival because it is nice",
+  "You will have a bad rest of the day.",
+  "dad is Nice!",
+  "you are given bad luck for the rest of your life HAHAHAHA!",
+  "max is Nice!",
+  "Jude is nice!"
+  
+] 
+
+print("==============")
+print("Fortune Teller")
+print("==============")
+
+fortune = random.choice(fortunes)
+print(fortune)
+
